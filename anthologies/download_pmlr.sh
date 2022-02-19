aaf3b9eac2d1821450398b4a20ca4e7b9778049e
tdsimao <tdsimao@users.noreply.github.com>
2022-02-19 09:59:57 +0100

    update pmlr url

--- anthologies/download_pmlr.sh
+++ anthologies/download_pmlr.sh
@@ -2,7 +2,7 @@
 
 # Proceedings of Machine Learning Research
 
-curl http://proceedings.mlr.press | grep -oP 'href="v[0-9]+"' | sed -e 's/href="//;s/"$//;s#^#http://proceedings.mlr.press/#;s#$#/bibliography.bib#' |\
+curl http://proceedings.mlr.press | grep -oP 'href="v[0-9]+"' | sed -e 's/href="//;s/"$//;s#^#http://proceedings.mlr.press/#;s#$#/assets/bib/bibliography.bib#' |\
 while read -r BIB_URL; do
 	echo $BIB_URL
 	curl $BIB_URL >> pmlr.bib
